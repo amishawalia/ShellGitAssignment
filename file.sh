29d5ea0096f4cceea65e9e95ff8be945b08dccbc
Amisha Walia <amisha1530.cse18@chitkara.edu.in>
2022-02-18 15:54:39 +0530

    remove commented code

--- file.sh
+++ file.sh
@@ -1,19 +1,3 @@
-# #!/bin/bash
-
-# for f in $@
-# do
-#  	OUTFILE=$1
-# 	SEARCH=$2
-# 	if [[-f $f]]; then
-# 		RESULT=`grep "$SEARCH" $f`
-# 	fi	
-# 	if [-n "$RESULT" ]; then
-# 		 echo -e "\n"
-# 	 	echo "Error in "$f" :"
-# 	 	echo $RESULT
-# 	 	echo -e "\n" >> $OUTFILE
-# 	fi
-# done
 #!/bin/bash
 expected=outfile
 for f in $@
@@ -23,8 +7,7 @@ do
   if [[ -f $f ]]; then
       RESULT=`grep "$SEARCH" $f`
   fi    
-      if [ -n "$RESULT" ]; then
-           
+      if [ -n "$RESULT" ]; then   
            echo -e "\n Error(s) in "$f":"
            echo $RESULT
            echo -e "\n" >> $OUTFILE
